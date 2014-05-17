e35683e07df8a9b384b83adb1af8283b0e52e6a9
Dylan Jew <d.jew@comcast.net>
2014-05-16 20:25:28 -0700

    Finished Chapter 6 exercises: Added more email tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -34,7 +34,7 @@ end
   describe "when email format is invalid" do
   	it "should be invalid" do
   		addresses = %w[user@foo,com user_at_foo.org example.user@foo.
-                     foo@bar_baz.com foo@bar+baz.com]
+                     foo@bar_baz.com foo@bar+baz.com user@foo..com]
         	addresses.each do |invalid_address|
         	@user.email = invalid_address
         	expect(@user).not_to be_valid
@@ -94,4 +94,15 @@ end
     		specify { expect(user_for_invalid_password).to be_false }
     	end
     end
+
+    describe "email address with mixed case" do
+    	let(:mixed_case_email) { "Foo@ExamPLe.cOM" }
+
+    	it "should be saved as all lower case" do
+    		@user.email = mixed_case_email
+    		@user.save
+    		expect(@user.reload.email).to eq mixed_case_email.downcase
+    	end
+    end
+
 end
