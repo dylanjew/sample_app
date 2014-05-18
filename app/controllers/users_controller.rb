2450f4edce44ca57328c8f0458b83e6f785c31c6
Dylan Jew <d.jew@comcast.net>
2014-05-18 13:44:10 -0700

    Part of updating users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,10 @@ class UsersController < ApplicationController
   	end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
   def new
   	@user = User.new
   end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,6 @@ SampleApp::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  "check_for_rvm": true,
 end
